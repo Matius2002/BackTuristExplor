67026eaee8286d15b3e68c575e278700d491a790
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:23:08 -0500

    Se agregan comentarios en RolNotFoundException

--- src/main/java/org/example/proyecturitsexplor/Excepciones/RolNotFoundException.java
+++ src/main/java/org/example/proyecturitsexplor/Excepciones/RolNotFoundException.java
@@ -1,13 +1,18 @@
 package org.example.proyecturitsexplor.Excepciones;
 
 public class RolNotFoundException extends RuntimeException {
+    // Atributo para almacenar el ID del rol no encontrado
     private Long id;
 
+    // Constructor que recibe el ID del rol no encontrado
+    // y pasa un mensaje personalizado a la clase RuntimeException
     public RolNotFoundException(Long id) {
-        super("Rol no encontrado: " + id);
-        this.id = id;
+        super("Rol no encontrado: " + id); // Llama al constructor de RuntimeException con un mensaje específico
+        this.id = id; // Asigna el ID recibido al atributo id
     }
+
+    // Método getter para obtener el ID del rol no encontrado
     public Long getId() {
-        return id;
+        return id; // Retorna el valor del atributo id
     }
 }
