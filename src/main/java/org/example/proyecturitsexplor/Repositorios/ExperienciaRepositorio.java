693a66ebbdd62ff45b22c44587a934d54a1756d3
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:35:22 -0500

    Se agregan comentarios en ExperienciaRepositorio

--- src/main/java/org/example/proyecturitsexplor/Repositorios/ExperienciaRepositorio.java
+++ src/main/java/org/example/proyecturitsexplor/Repositorios/ExperienciaRepositorio.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface ExperienciaRepositorio  extends JpaRepository<Experiencia, Long> {
-
+public interface ExperienciaRepositorio extends JpaRepository<Experiencia, Long> {
+    // Este es un repositorio que extiende JpaRepository para la entidad Experiencia.
+    // Proporciona métodos CRUD estándar como save(), findById(), findAll(), delete(), entre otros.
 }
