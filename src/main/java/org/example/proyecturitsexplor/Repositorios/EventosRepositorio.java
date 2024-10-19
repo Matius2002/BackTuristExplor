68881058e05e5d07680ead04f53ccae40ef55fc5
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:34:06 -0500

    Se agregan comentarios en EventosRepositorio

--- src/main/java/org/example/proyecturitsexplor/Repositorios/EventosRepositorio.java
+++ src/main/java/org/example/proyecturitsexplor/Repositorios/EventosRepositorio.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface EventosRepositorio  extends JpaRepository<Evento, Long> {
+public interface EventosRepositorio extends JpaRepository<Evento, Long> {
+    // Metodo para verificar si existe un Evento por su nombre
+    // Este metodo consulta si hay alguna entidad Evento con el nombre proporcionado
     boolean existsByNombre(String nombre);
 }
