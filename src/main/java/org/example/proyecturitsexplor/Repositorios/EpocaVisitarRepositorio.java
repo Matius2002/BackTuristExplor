33906525d814449a09d92a8ddfc1d1711a19945e
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:32:54 -0500

    Se agregan comentarios en EpocaVisitarRepositorio

--- src/main/java/org/example/proyecturitsexplor/Repositorios/EpocaVisitarRepositorio.java
+++ src/main/java/org/example/proyecturitsexplor/Repositorios/EpocaVisitarRepositorio.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface EpocaVisitarRepositorio  extends JpaRepository<EpocaVisitar, Long> {
-
+public interface EpocaVisitarRepositorio extends JpaRepository<EpocaVisitar, Long> {
+    // Metodo para verificar si existe una EpocaVisitar por su nombre
+    // Este metodo consulta si hay alguna entidad EpocaVisitar con el nombre proporcionado
     boolean existsByNombre(String nombre);
 }
