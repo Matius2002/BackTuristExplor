f2ae8b5917dcc2b3a44d0757bce87b70246020ce
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:21:49 -0500

    Se agregan comentarios en NoticiasNotFoundException

--- src/main/java/org/example/proyecturitsexplor/Excepciones/NoticiasNotFoundException.java
+++ src/main/java/org/example/proyecturitsexplor/Excepciones/NoticiasNotFoundException.java
@@ -1,14 +1,18 @@
 package org.example.proyecturitsexplor.Excepciones;
 
-public class NoticiasNotFoundException extends RuntimeException{
+public class NoticiasNotFoundException extends RuntimeException {
+    // Atributo para almacenar el ID de la noticia no encontrada
     private Long id;
 
+    // Constructor que recibe el ID de la noticia no encontrada
+    // y pasa un mensaje personalizado a la clase RuntimeException
     public NoticiasNotFoundException(Long id) {
-        super("Noticias no encontrado: " + id);
-        this.id = id;
-
+        super("Noticias no encontrada: " + id); // Llama al constructor de RuntimeException con un mensaje específico
+        this.id = id; // Asigna el ID recibido al atributo id
     }
+
+    // Método getter para obtener el ID de la noticia no encontrada
     public Long getId() {
-        return id;
+        return id; // Retorna el valor del atributo id
     }
 }
