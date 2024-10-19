20bdcd416e8e0c09a9c3d35ad0c6f8fa4f973e07
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:53:53 -0500

    Se agregan comentarios en HttpSecurityConfig

--- src/main/java/org/example/proyecturitsexplor/Seguridad/HttpSecurityConfig.java
+++ src/main/java/org/example/proyecturitsexplor/Seguridad/HttpSecurityConfig.java
@@ -1,4 +1,5 @@
 package org.example.proyecturitsexplor.Seguridad;
+
 import org.example.proyecturitsexplor.Seguridad.filter.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -18,26 +19,34 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class HttpSecurityConfig {
-    @Autowired
-    private AuthenticationProvider authProvider;
-    @Autowired
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
-    @Autowired
-    private AuthenticationEntryPoint authenticationEntryPoint;
-    @Autowired
-    private AccessDeniedHandler accessDeniedHandler;
 
+    @Autowired
+    private AuthenticationProvider authProvider;  // Proveedor de autenticación personalizado
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;  // Filtro de autenticación JWT
+    @Autowired
+    private AuthenticationEntryPoint authenticationEntryPoint;  // Maneja la respuesta cuando ocurre un error de autenticación
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;  // Maneja la respuesta cuando un usuario no tiene permisos para una acción
+
+    /**
+     * Configura la cadena de filtros de seguridad HTTP.
+     *
+     * @param http Configuración de HttpSecurity
+     * @return La configuración de seguridad para las solicitudes HTTP
+     * @throws Exception Excepción que puede ocurrir durante la configuración de seguridad
+     */
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(exceptionConfig->{
-                    exceptionConfig.authenticationEntryPoint(authenticationEntryPoint);
-                    exceptionConfig.accessDeniedHandler(accessDeniedHandler);
+        return http.csrf(AbstractHttpConfigurer::disable)  // Desactiva CSRF (Cross-Site Request Forgery)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // Configura el manejo de sesiones como "stateless", lo que significa que el servidor no guardará información de sesión.
+                .authenticationProvider(authProvider)  // Configura un proveedor de autenticación personalizado
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)  // Agrega el filtro JWT antes del filtro de autenticación de nombre de usuario y contraseña.
+                .exceptionHandling(exceptionConfig -> {
+                    exceptionConfig.authenticationEntryPoint(authenticationEntryPoint);  // Maneja la respuesta cuando ocurre un error de autenticación
+                    exceptionConfig.accessDeniedHandler(accessDeniedHandler);  // Maneja la respuesta cuando un usuario no tiene los permisos necesarios para una operación
                 })
-                .build();
+                .build();  // Construye la configuración
     }
 }
 
