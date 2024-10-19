d6f1cde8d07e9dbcf02a7f57dfff41a969a8da2c
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:18:11 -0500

    Se agregan comentarios en ExperienciaNotFoundException

--- src/main/java/org/example/proyecturitsexplor/Excepciones/ExperienciaNotFoundException.java
+++ src/main/java/org/example/proyecturitsexplor/Excepciones/ExperienciaNotFoundException.java
@@ -1,14 +1,18 @@
 package org.example.proyecturitsexplor.Excepciones;
 
-public class ExperienciaNotFoundException extends RuntimeException{
+public class ExperienciaNotFoundException extends RuntimeException {
+    // Atributo para almacenar el ID de la experiencia no encontrada
     private Long id;
 
+    // Constructor que recibe el ID de la experiencia no encontrada
+    // y pasa un mensaje personalizado a la clase RuntimeException
     public ExperienciaNotFoundException(Long id) {
-        super("Experiencia no encontrado: " + id);
-        this.id = id;
-
+        super("Experiencia no encontrada: " + id); // Llama al constructor de RuntimeException con un mensaje específico
+        this.id = id; // Asigna el ID recibido al atributo id
     }
+
+    // Método getter para obtener el ID de la experiencia no encontrada
     public Long getId() {
-        return id;
+        return id; // Retorna el valor del atributo id
     }
 }
