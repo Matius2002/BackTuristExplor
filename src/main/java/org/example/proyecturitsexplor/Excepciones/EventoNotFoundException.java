26c7c6355618a55e4735e6b1c893dfc4ed633fa6
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:12:37 -0500

    Se agregan comentarios en EventoNotFoundException

--- src/main/java/org/example/proyecturitsexplor/Excepciones/EventoNotFoundException.java
+++ src/main/java/org/example/proyecturitsexplor/Excepciones/EventoNotFoundException.java
@@ -1,14 +1,19 @@
 package org.example.proyecturitsexplor.Excepciones;
 
-public class EventoNotFoundException extends RuntimeException{
+public class EventoNotFoundException extends RuntimeException {
+    // Atributo para almacenar el ID del evento no encontrado
     private Long id;
 
+    // Constructor que recibe el ID del evento no encontrado
+    // y pasa un mensaje personalizado a la clase RuntimeException
     public EventoNotFoundException(Long id) {
-        super("Evento no encontrado: " + id);
-        this.id = id;
-
+        super("Evento no encontrado: " + id); // Llama al constructor de RuntimeException con un mensaje específico
+        this.id = id; // Asigna el ID recibido al atributo id
     }
+
+    // Método getter para obtener el ID del evento no encontrado
     public Long getId() {
-        return id;
+        return id; // Retorna el valor del atributo id
     }
 }
+
