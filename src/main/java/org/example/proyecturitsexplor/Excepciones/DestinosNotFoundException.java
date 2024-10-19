581c9407f77a1f13c2d9cbe1709d66d12f503a9d
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:09:43 -0500

    Se agregan comentarios en DestinosNotFoundException

--- src/main/java/org/example/proyecturitsexplor/Excepciones/DestinosNotFoundException.java
+++ src/main/java/org/example/proyecturitsexplor/Excepciones/DestinosNotFoundException.java
@@ -1,14 +1,19 @@
 package org.example.proyecturitsexplor.Excepciones;
 
 public class DestinosNotFoundException extends RuntimeException {
+    // Atributo para almacenar el ID del destino no encontrado
     private Long id;
 
+    // Constructor que recibe el ID del destino no encontrado
+    // y pasa un mensaje personalizado a la clase RuntimeException
     public DestinosNotFoundException(Long id) {
-        super("Destino no encontrado: " + id);
-        this.id = id;
-
+        super("Destino no encontrado: " + id); // Llama al constructor de RuntimeException con un mensaje específico
+        this.id = id; // Asigna el ID recibido al atributo id
     }
+
+    // Método getter para obtener el ID del destino no encontrado
     public Long getId() {
-        return id;
+        return id; // Retorna el valor del atributo id
     }
 }
+
