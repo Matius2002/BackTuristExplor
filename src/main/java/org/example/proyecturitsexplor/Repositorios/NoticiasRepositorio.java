0b87a8fe1768b992a2c6766fd42a8cae4d713e0a
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:37:34 -0500

    Se agregan comentarios en NoticiasRepositorio

--- src/main/java/org/example/proyecturitsexplor/Repositorios/NoticiasRepositorio.java
+++ src/main/java/org/example/proyecturitsexplor/Repositorios/NoticiasRepositorio.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface NoticiasRepositorio  extends JpaRepository<Noticia, Long> {
+public interface NoticiasRepositorio extends JpaRepository<Noticia, Long> {
+    // Metodo para verificar si existe una Noticia por su título
+    // Este metodo consulta si hay alguna entidad Noticia con el título proporcionado
     boolean existsByTitulo(String titulo);
 }
