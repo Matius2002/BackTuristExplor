ed15546d12e54f984c348949fbff850690f9ae87
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:02:31 -0500

    Se agregan comentarios en AlojamientoNotFoundException

--- src/main/java/org/example/proyecturitsexplor/Excepciones/AlojamientoNotFoundException.java
+++ src/main/java/org/example/proyecturitsexplor/Excepciones/AlojamientoNotFoundException.java
@@ -1,14 +1,18 @@
 package org.example.proyecturitsexplor.Excepciones;
 
-public class AlojamientoNotFoundException extends RuntimeException{
+public class AlojamientoNotFoundException extends RuntimeException {
+    // Atributo para almacenar el ID del alojamiento no encontrado
     private Long id;
 
+    // Constructor que recibe el ID del alojamiento no encontrado
+    // y pasa un mensaje personalizado a la clase RuntimeException
     public AlojamientoNotFoundException(Long id) {
-        super("Alojamiento no encontrado: " + id);
-        this.id = id;
-
+        super("Alojamiento no encontrado: " + id); // Llama al constructor de RuntimeException con un mensaje específico
+        this.id = id; // Asigna el ID recibido al atributo id
     }
+
+    // Método getter para obtener el ID del alojamiento no encontrado
     public Long getId() {
-        return id;
+        return id; // Retorna el valor del atributo id
     }
 }
