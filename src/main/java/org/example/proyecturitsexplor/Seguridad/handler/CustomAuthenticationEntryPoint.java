510867e796c9c87abf3c88f80ffb86cbf455002e
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:50:31 -0500

    Se agregan comentarios en CustomAuthenticationEntryPoint

--- src/main/java/org/example/proyecturitsexplor/Seguridad/handler/CustomAuthenticationEntryPoint.java
+++ src/main/java/org/example/proyecturitsexplor/Seguridad/handler/CustomAuthenticationEntryPoint.java
@@ -16,13 +16,33 @@ import java.io.IOException;
 
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    // ObjectMapper para convertir el DTO en JSON
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+    /**
+     * Este metodo maneja los casos en los que un usuario intenta acceder a un recurso sin estar autenticado.
+     * Se llama automáticamente cuando ocurre una excepción de autenticación, como un fallo en el inicio de sesión.
+     *
+     * @param request La solicitud HTTP entrante.
+     * @param response La respuesta HTTP.
+     * @param authException Excepción lanzada cuando no hay autenticación válida.
+     * @throws IOException Si ocurre un error al escribir la respuesta.
+     * @throws ServletException Si ocurre un error en el manejo de la solicitud.
+     */
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        // Se crea el DTO que contiene los detalles de la excepción de autenticación fallida
         GenericBussinessSecurityExceptionDTO exceptionDTO = new GenericBussinessSecurityExceptionDTO
                 ("Error de inicio de sesión", "401", "Aún no ha iniciado sesión, por favor, inicie sesión para continuar", "AuthenticationException");
+
+        // Se configura la respuesta con el tipo de contenido JSON
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        // Se establece el código de estado HTTP 401 (Unauthorized)
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        // Se convierte el DTO a JSON y se escribe en la respuesta
         response.getOutputStream().write(objectMapper.writeValueAsBytes(exceptionDTO));
     }
 }
