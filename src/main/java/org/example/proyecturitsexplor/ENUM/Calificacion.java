c33d3667dde12622eabc49399fb3c543b037a3a7
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 00:24:43 -0500

    Se agrega comentarios en Calificación

--- src/main/java/org/example/proyecturitsexplor/ENUM/Calificacion.java
+++ src/main/java/org/example/proyecturitsexplor/ENUM/Calificacion.java
@@ -1,9 +1,11 @@
 package org.example.proyecturitsexplor.ENUM;
 
+// Definición de la enumeración Calificacion
 public enum Calificacion {
-    Excelente,
-    Bueno,
-    Regular,
-    Malo,
-    Muy_Malo
+    Excelente,  // Representa una calificación excelente
+    Bueno,      // Representa una calificación buena
+    Regular,    // Representa una calificación regular
+    Malo,       // Representa una calificación mala
+    Muy_Malo    // Representa una calificación muy mala
 }
+
