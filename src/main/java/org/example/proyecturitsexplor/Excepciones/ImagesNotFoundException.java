bf19ac659aa4785b2f56b8ebb0c59a2f4f12ebb2
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:19:38 -0500

    Se agregan comentarios en ImagenesNotFoundException

--- src/main/java/org/example/proyecturitsexplor/Excepciones/ImagesNotFoundException.java
+++ src/main/java/org/example/proyecturitsexplor/Excepciones/ImagesNotFoundException.java
@@ -1,14 +1,18 @@
 package org.example.proyecturitsexplor.Excepciones;
 
-public class ImagesNotFoundException  extends RuntimeException{
+public class ImagesNotFoundException extends RuntimeException {
+    // Atributo para almacenar el ID de las imágenes no encontradas
     private Long id;
 
+    // Constructor que recibe el ID de las imágenes no encontradas
+    // y pasa un mensaje personalizado a la clase RuntimeException
     public ImagesNotFoundException(Long id) {
-        super("Images no encontrado: " + id);
-        this.id = id;
-
+        super("Imágenes no encontradas: " + id); // Llama al constructor de RuntimeException con un mensaje específico
+        this.id = id; // Asigna el ID recibido al atributo id
     }
+
+    // Método getter para obtener el ID de las imágenes no encontradas
     public Long getId() {
-        return id;
+        return id; // Retorna el valor del atributo id
     }
 }
