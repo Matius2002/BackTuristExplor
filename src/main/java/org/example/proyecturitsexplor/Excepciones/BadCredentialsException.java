afec6c60a4b3a52bb7450c2f14a98cda8eb62276
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:06:38 -0500

    Se agregan comentarios en BadCredentialsException

--- src/main/java/org/example/proyecturitsexplor/Excepciones/BadCredentialsException.java
+++ src/main/java/org/example/proyecturitsexplor/Excepciones/BadCredentialsException.java
@@ -2,11 +2,11 @@ package org.example.proyecturitsexplor.Excepciones;
 
 import org.springframework.http.HttpStatus;
 
-public class BadCredentialsException extends BussinessSecurityException{
+public class BadCredentialsException extends BussinessSecurityException {
 
+    // Constructor que recibe un mensaje, un código, un título y un estado HTTP
     public BadCredentialsException(String message, String code, String titulo, HttpStatus httpStatus) {
+        // Llama al constructor de la clase base BussinessSecurityException con los parámetros proporcionados
         super(code, titulo, message, httpStatus);
     }
-
-
 }
