ae0d3484e1ec197e2bc2f86ba78e17a54761d8ef
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:43:20 -0500

    Se agregan comentarios en UserRepositorio

--- src/main/java/org/example/proyecturitsexplor/Repositorios/UserRepositorio.java
+++ src/main/java/org/example/proyecturitsexplor/Repositorios/UserRepositorio.java
@@ -7,10 +7,21 @@ import java.util.Optional;
 
 @Service
 public interface UserRepositorio extends JpaRepository<Usuarios, Long> {
+
+    // Metodo para verificar si existe un usuario por su nombre de usuario
+    // Este metodo consulta si hay alguna entidad Usuarios con el nombre de usuario proporcionado
     boolean existsByNombreUsuario(String nombreUsuario);
+
+    // Metodo para verificar si existe un usuario por su email
+    // Este metodo consulta si hay alguna entidad Usuarios con el email proporcionado
     boolean existsByEmail(String email);
 
+    // Metodo para encontrar un usuario por su email
+    // Devuelve un Optional que contiene el usuario si se encuentra, o vacío si no existe
     Optional<Usuarios> findByEmail(String email);
 
+    // Metodo para encontrar un usuario por su nombre de usuario
+    // Devuelve la entidad Usuarios asociada al nombre de usuario proporcionado
     Usuarios findByNombreUsuario(String nombreUsuario);
 }
+
