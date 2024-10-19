80ebed046dab22f66b2b01e54695d8fcdc583511
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:36:27 -0500

    Se agregan comentarios en ImagesRepositorio

--- src/main/java/org/example/proyecturitsexplor/Repositorios/ImagesRepositorio.java
+++ src/main/java/org/example/proyecturitsexplor/Repositorios/ImagesRepositorio.java
@@ -3,6 +3,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.example.proyecturitsexplor.Entidades.Images;
 
 public interface ImagesRepositorio extends JpaRepository<Images, Long> {
-
+    // Metodo para verificar si existe una imagen por su nombre
+    // Este metodo consulta si hay alguna entidad Images con el nombre proporcionado
     boolean existsByNombre(String nombre);
 }
