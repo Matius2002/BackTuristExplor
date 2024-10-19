3c888e7ef7f711d3916d67f60bbd28fa8e85e6df
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:30:17 -0500

    Se agregan comentarios en AuditoriaRepositorio

--- src/main/java/org/example/proyecturitsexplor/Repositorios/AuditoriaRepositorio.java
+++ src/main/java/org/example/proyecturitsexplor/Repositorios/AuditoriaRepositorio.java
@@ -3,4 +3,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface AuditoriaRepositorio {
+    // Esta es una interfaz de repositorio vacía que puede ser utilizada para operaciones de auditoría
+    // Se espera que se implemente en el futuro para realizar operaciones relacionadas con la auditoría
 }
+
