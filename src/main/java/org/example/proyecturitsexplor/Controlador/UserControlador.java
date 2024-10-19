6be649ad38f665a04a72688f2923e867843671e2
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-18 23:37:52 -0500

    Se agrega comentarios en UserControlador

--- src/main/java/org/example/proyecturitsexplor/Controlador/UserControlador.java
+++ src/main/java/org/example/proyecturitsexplor/Controlador/UserControlador.java
@@ -1,4 +1,5 @@
 package org.example.proyecturitsexplor.Controlador;
+
 import jakarta.servlet.http.HttpServletRequest;
 import org.example.proyecturitsexplor.Entidades.Usuarios;
 import org.example.proyecturitsexplor.Repositorios.UserRepositorio;
@@ -17,55 +18,59 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/api")
-@CrossOrigin (origins = "http://localhost:8080")
-//(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:8080")
 public class UserControlador {
+
     @Autowired
     private UserRepositorio userRepositorio;
+
     @Autowired
     private UserServicio userServicio;
+
     @Autowired
     private AuthenticationService authenticationService;
 
     // CRUD para Usuarios
 
-    //CONTROLADOR LIBRE
+    // CONTROLADOR LIBRE
     @PreAuthorize("permitAll()")
     @PostMapping("/usuarios/guardarUsuario")
     public ResponseEntity<Usuarios> guardarUsuario(@RequestBody Usuarios usuarios) {
-        if (usuarios.getNombreUsuario()==null || usuarios.getEmail()==null ||
-                usuarios.getRoles()==null || usuarios.getPassword()==null){
-            return  ResponseEntity.badRequest().build();
+        if (usuarios.getNombreUsuario() == null ||
+                usuarios.getEmail() == null ||
+                usuarios.getRoles() == null ||
+                usuarios.getPassword() == null) {
+            return ResponseEntity.badRequest().build();
         }
+
         Usuarios usuarioGuardado = userServicio.guardarUsuarios(usuarios);
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioGuardado);
     }
 
-    //Recuperar todos los Usuarios
+    // Recuperar todos los Usuarios
     @GetMapping("/usuarios/obtenerTodosLosUsuario")
     public ResponseEntity<List<Usuarios>> obtenerTodosLosUsuarios() {
         List<Usuarios> usuarios = userServicio.obtenerTodosLosUsuarios();
         return ResponseEntity.ok(usuarios);
     }
 
-    //Recuperar Usuarios por id
+    // Recuperar Usuarios por id
     @GetMapping("/usuarios/recuperarPorId/{id}")
-    public ResponseEntity<Usuarios>obtenerUsuariosPorId(@PathVariable Long id){
+    public ResponseEntity<Usuarios> obtenerUsuariosPorId(@PathVariable Long id) {
         Usuarios usuarios = userServicio.obtenerUsuariosPorId(id);
         return ResponseEntity.ok(usuarios);
     }
 
-    //Actulizar Usuarios
+    // Actualizar Usuarios
     @PutMapping("/usuarios/{id}")
     public ResponseEntity<?> actualizarUsuarios(@PathVariable("id") Long id, @RequestBody Usuarios usuarioActualizada) {
         try {
-
             if (!id.equals(usuarioActualizada.getId())) {
-                throw new IllegalArgumentException("El ID del Usuario principal del cuerpo no coincide con el ID proporcionado en la ruta.");
+                throw new IllegalArgumentException("El ID del Usuario del cuerpo no coincide con el ID proporcionado en la ruta.");
             }
             Usuarios usuarioActual = userServicio.obtenerUsuariosPorId(id);
             if (usuarioActual == null) {
-                return new ResponseEntity<>("No se encontró ningun Usuario con el ID proporcionado.", HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>("No se encontró ningún Usuario con el ID proporcionado.", HttpStatus.NOT_FOUND);
             }
             Usuarios usuarioGuardada = userServicio.actulizarUsuarios(usuarioActualizada);
             return new ResponseEntity<>(usuarioGuardada, HttpStatus.OK);
@@ -74,14 +79,14 @@ public class UserControlador {
         }
     }
 
-    //Eliminar Usuario
+    // Eliminar Usuario
     @DeleteMapping("/usuarios/{id}")
     public ResponseEntity<String> eliminarUsuarioPorId(@PathVariable Long id) {
         userServicio.eliminarUsuarios(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Usuario con ID " + id + " eliminada correctamente.");
     }
 
-    //Verificar si un Usuario  existe en la base de datos
+    // Verificar si un Usuario existe en la base de datos
     @GetMapping("/usuarios/existe/{nombreUsuario}")
     public ResponseEntity<?> verificarUsuarioExistente(@PathVariable String nombreUsuario) {
         try {
@@ -91,6 +96,7 @@ public class UserControlador {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al verificar si el Usuario existe por Nombre: " + e.getMessage());
         }
     }
+
     @GetMapping("/usuarios/userExiste/{email}")
     public ResponseEntity<?> verificarExistentePorEmail(@PathVariable String email) {
         try {
@@ -102,7 +108,7 @@ public class UserControlador {
     }
 
     @GetMapping("/usuarios/profile")
-    public ResponseEntity<?> verPerfil(){
+    public ResponseEntity<?> verPerfil() {
         return ResponseEntity.ok(this.authenticationService.findLoggedUser());
     }
 
@@ -130,7 +136,4 @@ public class UserControlador {
 
         return ResponseEntity.ok(response);
     }
-
-
 }
-
