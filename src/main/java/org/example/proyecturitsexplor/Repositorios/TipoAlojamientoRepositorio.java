04e31430547b69fa3672074e351fff3de240724d
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:40:51 -0500

    Se agregan comentarios en TipoAlojamientoRepositorio

--- src/main/java/org/example/proyecturitsexplor/Repositorios/TipoAlojamientoRepositorio.java
+++ src/main/java/org/example/proyecturitsexplor/Repositorios/TipoAlojamientoRepositorio.java
@@ -1,8 +1,11 @@
 package org.example.proyecturitsexplor.Repositorios;
+
 import org.example.proyecturitsexplor.Entidades.TipoAlojamiento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TipoAlojamientoRepositorio  extends JpaRepository<TipoAlojamiento, Long> {
+public interface TipoAlojamientoRepositorio extends JpaRepository<TipoAlojamiento, Long> {
 
+    // Metodo para verificar si existe un TipoAlojamiento por su nombre
+    // Este metodo consulta si hay alguna entidad TipoAlojamiento con el nombre proporcionado
     boolean existsByNombre(String nombre);
 }
