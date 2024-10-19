a77c07ddbb2b1c7ddcda18200bbe87458a2cef05
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 11:02:22 -0500

    Se agregan comentarios en SecretKey

--- src/main/java/org/example/proyecturitsexplor/Servicios/auth/SecretKey.java
+++ src/main/java/org/example/proyecturitsexplor/Servicios/auth/SecretKey.java
@@ -1,6 +1,9 @@
 package org.example.proyecturitsexplor.Servicios.auth;
 
 public class SecretKey {
+
+    // La clave secreta utilizada para la firma del JWT, codificada en Base64
+    // Se trata de una cadena larga que se utilizará para la firma y validación de los tokens.
     public String secretKey="ZXN0YSBlcyB1bmEgY2xhdmUgc2VjcmV0ZSBlbmNyaXB0YWRhIGVuIGJhc2U2" +
             "NCgoanN3b2lqZGVkd2xtbDNybGVtdmw0dGtlbXZsa2V2bGVydmpscm5janduZWNqd2VqY3duamtjbn" +
             "dranJjd2tqbmRjd2tqZWRlamRsZWpqd2VsY2p3bGpjbndqY3dqZGx3ZWpka3dlw7F3a3JqaWxkamlq" +
@@ -9,11 +12,18 @@ public class SecretKey {
             "nbDsXbDsXtydnJqdm5qZW5qZW5qa2Nud2tqY25ld2tqZWNua2puY2plbnZqbXZrXG5od3NiaGpxYmRoc" +
             "WJoZWJcXFxzdndxZ2RncWhic3VicWhiaHx8fHxoaGVkYmVoMmRud2tqZWNuZWtqcm5ranJlbmZramVyb" +
             "nZqa2VudmplcnZlanZrd2Nrb2RsY2xjcGVsdsOpw7F2w6nDqXBmdsOpa3ZqZWp2ZWx2amVuY2x3bmp3bm" +
-            "NrandueGtqd2RueGp3ZGNid2h2aHVyaml3ZWplaHlxMzMxMzFyaGpkbjJlbmbDscOxw7HDscOxw7HDscOx" +
+            "NrandueXtqd2RueGp3ZGNid2h2aHVyaml3ZWplaHlxMzMxMzFyaGpkbjJlbmbDscOxw7HDscOxw7HDscOx" +
             "w7HDscOxw7HDscOxw7FkZXlqMmdkY2hqYmNoajNyYmZoM3Juam5qa25mamszYmRoZXZnY2ZzeGF6Pj4+Pjx" +
             "zMmVqZHJuZWpya252amt0NG52amt0bmJqdGJ0cnZrcmVvcGtyd29wZmtyaWZ1dHV1dXV1dXV1dXV1dXV1dXV1dX" +
             "V1M3Jqa3J1M2JocmJoamJocmJjZXJoa2NiZWhjYmNocmJjaGNiaGNiaHJoY2JlaDE0NDEyNjI3ODM5MzA0wr8rKy" +
             "srK3Nkb2tkbWt2bWVrdm10a2JtYm15cGttamJhemF6ZmFhamtkZirCqMKowqhkZXVpZmhyZm4zaGY0ZmkzaGZ1M2h" +
             "mazRqaGhmaHVqcmpqdG5yaHUzaHV5cnV3eXVleXd1eXV3eXVleXd1aGtqd3Z2KSlmaW4gZGUgbGEgY2xhdmUgc2Vjc" +
             "mV0YSBlbmNyaXB0YWRhIGVuIGJhc2U2NA==";
+
+    /**
+     * El campo `secretKey` contiene la clave secreta que se utiliza para la firma de los JWTs (JSON Web Tokens).
+     * Esta clave debe ser almacenada de forma segura y su valor debe mantenerse en secreto.
+     * Se ha codificado en Base64 para facilitar su almacenamiento y uso en aplicaciones.
+     * El valor específico aquí es solo un ejemplo, y en un entorno de producción, se debe generar una clave secreta segura y robusta.
+     */
 }
