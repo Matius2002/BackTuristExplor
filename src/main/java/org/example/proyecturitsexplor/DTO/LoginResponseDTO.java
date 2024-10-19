b64d6d22a46f120ef24d58ee5194888d0fb8f9fa
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-18 23:48:30 -0500

    Se agrega comentarios en LoginResponseDTO

--- src/main/java/org/example/proyecturitsexplor/DTO/LoginResponseDTO.java
+++ src/main/java/org/example/proyecturitsexplor/DTO/LoginResponseDTO.java
@@ -2,19 +2,25 @@ package org.example.proyecturitsexplor.DTO;
 
 public class LoginResponseDTO {
 
+    // Token de autenticación generado para el usuario al iniciar sesión
     private String token;
 
+    // Constructor por defecto
     public LoginResponseDTO() {}
 
+    // Constructor que inicializa el token
     public LoginResponseDTO(String token) {
-        this.token = token;
+        this.token = token; // Asigna el token a la propiedad
     }
 
+    // Getter para el token
     public String getToken() {
-        return token;
+        return token; // Devuelve el token de autenticación
     }
 
+    // Setter para el token
     public void setToken(String token) {
-        this.token = token;
+        this.token = token; // Establece el token de autenticación
     }
 }
+
