dbf99418ea8af845d39fb16b8587667faf9ecd88
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:24:15 -0500

    Se agregan comentarios en TipoAÃlojamientoNotFoundException

--- src/main/java/org/example/proyecturitsexplor/Excepciones/TipoAlojamientoNotFoundException.java
+++ src/main/java/org/example/proyecturitsexplor/Excepciones/TipoAlojamientoNotFoundException.java
@@ -1,12 +1,18 @@
 package org.example.proyecturitsexplor.Excepciones;
 
-public class TipoAlojamientoNotFoundException extends RuntimeException{
+public class TipoAlojamientoNotFoundException extends RuntimeException {
+    // Atributo para almacenar el ID del tipo de alojamiento no encontrado
     private Long id;
+
+    // Constructor que recibe el ID del tipo de alojamiento no encontrado
+    // y pasa un mensaje personalizado a la clase RuntimeException
     public TipoAlojamientoNotFoundException(Long id) {
-        super("tipo de alojamiento no encontrado: " + id);
-        this.id = id;
+        super("Tipo de alojamiento no encontrado: " + id); // Llama al constructor de RuntimeException con un mensaje específico
+        this.id = id; // Asigna el ID recibido al atributo id
     }
+
+    // Método getter para obtener el ID del tipo de alojamiento no encontrado
     public Long getId() {
-        return id;
+        return id; // Retorna el valor del atributo id
     }
 }
