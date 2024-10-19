6603b24ab3d44bfe9df52a4586b83f999199e29a
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:25:39 -0500

    Se agregan comentarios en TipoTurismoNotFoundException

--- src/main/java/org/example/proyecturitsexplor/Excepciones/TipoTurismoNotFoundException.java
+++ src/main/java/org/example/proyecturitsexplor/Excepciones/TipoTurismoNotFoundException.java
@@ -1,11 +1,18 @@
 package org.example.proyecturitsexplor.Excepciones;
 
-public class TipoTurismoNotFoundException extends RuntimeException{
+public class TipoTurismoNotFoundException extends RuntimeException {
+    // Atributo para almacenar el ID del tipo de turismo no encontrado
     private Long id;
 
-    public TipoTurismoNotFoundException(Long id){
-        super("TipoTurismo no encontrado: " + id);
-        this.id = id;
+    // Constructor que recibe el ID del tipo de turismo no encontrado
+    // y pasa un mensaje personalizado a la clase RuntimeException
+    public TipoTurismoNotFoundException(Long id) {
+        super("TipoTurismo no encontrado: " + id); // Llama al constructor de RuntimeException con un mensaje específico
+        this.id = id; // Asigna el ID recibido al atributo id
     }
-    public Long getId(){return id;}
+
+    // Método getter para obtener el ID del tipo de turismo no encontrado
+    public Long getId() {
+        return id; // Retorna el valor del atributo id
+    }
 }
