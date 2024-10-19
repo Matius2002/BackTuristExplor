5e8a1897fa7b9bc68584e7c7e8a04eee14d51cea
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:26:45 -0500

    Se agregan comentarios en UserNotFoundException

--- src/main/java/org/example/proyecturitsexplor/Excepciones/UserNotFoundException.java
+++ src/main/java/org/example/proyecturitsexplor/Excepciones/UserNotFoundException.java
@@ -1,13 +1,16 @@
 package org.example.proyecturitsexplor.Excepciones;
 
 public class UserNotFoundException extends RuntimeException {
+    // Constructor que recibe el email del usuario no encontrado
+    // y pasa un mensaje personalizado a la clase RuntimeException
     public UserNotFoundException(String email) {
-        super("User not found with email: " + email);
+        super("User not found with email: " + email); // Llama al constructor de RuntimeException con el mensaje específico
     }
 
+    // Constructor que recibe el ID del usuario no encontrado
+    // y pasa un mensaje personalizado a la clase RuntimeException
     public UserNotFoundException(Long id) {
-        super("User not found with id: " + id);
+        super("User not found with id: " + id); // Llama al constructor de RuntimeException con el mensaje específico
     }
-
-
 }
+
