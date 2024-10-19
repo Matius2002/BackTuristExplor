263e5cecfdade9d4ef84a8c2dd5880e4584a32d8
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:05:20 -0500

    Se agregan comentarios en AtracionPrincipalNotFoundException

--- src/main/java/org/example/proyecturitsexplor/Excepciones/AtracionPrincipalNotFoundException.java
+++ src/main/java/org/example/proyecturitsexplor/Excepciones/AtracionPrincipalNotFoundException.java
@@ -1,13 +1,19 @@
 package org.example.proyecturitsexplor.Excepciones;
 
 public class AtracionPrincipalNotFoundException extends RuntimeException {
+    // Atributo para almacenar el ID de la atracción principal no encontrada
     private Long id;
 
+    // Constructor que recibe el ID de la atracción principal no encontrada
+    // y pasa un mensaje personalizado a la clase RuntimeException
     public AtracionPrincipalNotFoundException(Long id) {
-        super("Atración Principal no encontrado: " + id);
-        this.id = id;
+        super("Atracción Principal no encontrado: " + id); // Llama al constructor de RuntimeException con un mensaje específico
+        this.id = id; // Asigna el ID recibido al atributo id
     }
+
+    // Método getter para obtener el ID de la atracción principal no encontrada
     public Long getId() {
-        return id;
+        return id; // Retorna el valor del atributo id
     }
 }
+
