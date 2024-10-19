7a5b91499d854373748bf9cc2ddedad46ea9d12f
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-19 10:41:57 -0500

    Se agregan comentarios en TipoTurismoRepositorio

--- src/main/java/org/example/proyecturitsexplor/Repositorios/TipoTurismoRepositorio.java
+++ src/main/java/org/example/proyecturitsexplor/Repositorios/TipoTurismoRepositorio.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 @Service
 public interface TipoTurismoRepositorio extends JpaRepository<TipoTurismo, Long> {
+
+    // Metodo para verificar si existe un TipoTurismo por su nombre
+    // Este metodo consulta si hay alguna entidad TipoTurismo con el nombre proporcionado
     boolean existsByNombre(String nombre);
 }
